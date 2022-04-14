e0b5854b358ecdcd50dc7f8bba202b8f9b0d71ed
Michael Scofield <mscofield0@tutanota.com>
2022-04-14 03:01:54 +0200

    Update interface of Index

--- src/index/mod.rs
+++ src/index/mod.rs
@@ -1,15 +1,16 @@
 mod parsing;
 
 use crate::DictError;
-use std::{collections::HashMap, io::BufRead};
+use std::{collections::HashMap, io::{Seek, Read, BufReader}};
 
 pub struct Index {
     pub words: HashMap<String, (u64, u64)>,
 }
 
 impl Index {
-    pub fn new<R: BufRead>(reader: R) -> Result<Self, DictError> {
-        parsing::parse(reader)
+    pub fn new<R: Read + Seek>(reader: R) -> Result<Self, DictError> {
+        let buf_reader = BufReader::new(reader);
+        parsing::parse(buf_reader)
     }
 }
 
